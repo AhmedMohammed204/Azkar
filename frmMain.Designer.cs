e6d85e965c8fc9018e3e453a7c4307ff42608168
dell <dell@DESKTOP-OB7FVAR>
2023-11-29 12:18:42 +0300

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Azkar
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbUserChoice = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudNumberOfMinutes = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblTimeDown = new System.Windows.Forms.Label();
+            this.timerTimeDown = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbUserChoice
+            // 
+            this.cbUserChoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbUserChoice.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbUserChoice.FormattingEnabled = true;
+            this.cbUserChoice.Items.AddRange(new object[] {
+            "الحمد لله",
+            "اللهم صل و سلم على نبينا محمد",
+            "سبحان الله",
+            "لا اله الا الله",
+            "عشوائي"});
+            this.cbUserChoice.Location = new System.Drawing.Point(97, 106);
+            this.cbUserChoice.Name = "cbUserChoice";
+            this.cbUserChoice.Size = new System.Drawing.Size(250, 24);
+            this.cbUserChoice.TabIndex = 0;
+            this.cbUserChoice.SelectedIndexChanged += new System.EventHandler(this.cbUserChoice_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(28, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Check an option";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(169, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 51);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Azkar";
+            // 
+            // nudNumberOfMinutes
+            // 
+            this.nudNumberOfMinutes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nudNumberOfMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudNumberOfMinutes.Location = new System.Drawing.Point(245, 156);
+            this.nudNumberOfMinutes.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumberOfMinutes.Name = "nudNumberOfMinutes";
+            this.nudNumberOfMinutes.Size = new System.Drawing.Size(102, 20);
+            this.nudNumberOfMinutes.TabIndex = 3;
+            this.nudNumberOfMinutes.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumberOfMinutes.ValueChanged += new System.EventHandler(this.nudNumberOfMinutes_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(29, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Time to send notifications";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(362, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "minute";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Khaki;
+            this.btnReset.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnReset.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft New Tai Lue", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(82, 252);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(108, 56);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.BackColor = System.Drawing.Color.Khaki;
+            this.btnDone.Enabled = false;
+            this.btnDone.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnDone.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.btnDone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDone.Font = new System.Drawing.Font("Microsoft New Tai Lue", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDone.Location = new System.Drawing.Point(257, 252);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(108, 56);
+            this.btnDone.TabIndex = 7;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = false;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // lblTimeDown
+            // 
+            this.lblTimeDown.AutoSize = true;
+            this.lblTimeDown.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeDown.Location = new System.Drawing.Point(366, 46);
+            this.lblTimeDown.Name = "lblTimeDown";
+            this.lblTimeDown.Size = new System.Drawing.Size(59, 16);
+            this.lblTimeDown.TabIndex = 8;
+            this.lblTimeDown.Text = "00:00:00";
+            // 
+            // timerTimeDown
+            // 
+            this.timerTimeDown.Interval = 1000;
+            this.timerTimeDown.Tick += new System.EventHandler(this.timerTimeDown_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Azkar.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(483, 320);
+            this.Controls.Add(this.lblTimeDown);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudNumberOfMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbUserChoice);
+            this.Name = "frmMain";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbUserChoice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudNumberOfMinutes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label lblTimeDown;
+        private System.Windows.Forms.Timer timerTimeDown;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Azkar
+{
+    public partial class frmMain : Form
+    {
+        struct stNotiData
+        {
+            public string Message;
+            public int TimeMS;
+        }
+        stNotiData NotiData = new stNotiData();
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void cbUserChoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NotiData.Message = cbUserChoice.Text;
+            btnDone.Enabled = true;
+
+        }
+
+        private void nudNumberOfMinutes_ValueChanged(object sender, EventArgs e)
+        {
+            NotiData.TimeMS = Convert.ToInt32(nudNumberOfMinutes.Value) * 60 * 1000;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            NotiData.Message = "";
+            nudNumberOfMinutes.Value = 1;
+            nudNumberOfMinutes.Enabled = true;
+
+            cbUserChoice.SelectedIndex = -1;
+            cbUserChoice.Enabled = true;
+
+            lblTimeDown.Text = "00:00:00";
+            timerTimeDown.Enabled = false;
+            timer1.Enabled = false;
+        }
+        void FillTimeSpan()
+        {
+            int Hours = Convert.ToInt32(nudNumberOfMinutes.Value) / 60;
+            int Minutes = Convert.ToInt32(nudNumberOfMinutes.Value) % 60;
+            ts = new TimeSpan(Hours, Minutes, 0);
+        }
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            if (cbUserChoice.SelectedIndex == -1)
+            {
+                MessageBox.Show("You have to chose an option", "Sorry", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            nudNumberOfMinutes.Enabled = false;
+            cbUserChoice.Enabled = false;
+            
+            FillTimeSpan();
+            timerTimeDown.Enabled = true;
+
+            timer1.Enabled = true;
+            timer1.Interval = NotiData.TimeMS;
+            
+        }
+
+        string NotificationMessage()
+        {
+            if(NotiData.Message != "عشوائي")
+                return NotiData.Message;
+
+            Random rd = new Random();
+
+            string Message = cbUserChoice.Items[rd.Next(0, cbUserChoice.Items.Count - 1)].ToString();
+
+            return Message;
+
+        }
+
+        void SendNotifaction()
+        {
+            notifyIcon1.Icon = SystemIcons.Information;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = "Azkar";
+            notifyIcon1.BalloonTipText = NotificationMessage();
+            notifyIcon1.ShowBalloonTip(1000);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendNotifaction();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            NotiData.TimeMS = 60 * 1000;
+        }
+
+
+
+
+        //Time Down
+
+
+        TimeSpan ts;
+        void UpdateTimeDown()
+        {
+            lblTimeDown.Text = ts.ToString();
+            ts -= new TimeSpan(0,0, 1);
+            if (ts == new TimeSpan(0, 0, 0))
+                FillTimeSpan();
+        }
+
+        private void timerTimeDown_Tick(object sender, EventArgs e)
+        {
+            UpdateTimeDown();
+        }
+    }
+}
